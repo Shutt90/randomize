81a6395db8eb30acb9e8e55785ab0e55dedb78f3
Shutt90 <78647249+Shutt90@users.noreply.github.com>
2022-12-20 20:29:25 +0000

    adding passwords with usernames and website names

--- main.go
+++ main.go
@@ -3,18 +3,60 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"strconv"
 	"strings"
 	"time"
 )
 
-func main() {
-	fmt.Println(randomize(16))
+type storedPassword struct {
+	WebsiteName string    `json:"websiteName"`
+	Username    string    `json:"username"`
+	Password    string    `json:"password"`
+	Created     time.Time `json:"created"`
 }
 
-func randomize(numLetters int) string {
+func main() {
+	fmt.Println("Enter website name: ")
+	var entry storedPassword
+	_, err := fmt.Scanln(&entry.WebsiteName)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Println("Enter username: ")
+	_, err = fmt.Scanln(&entry.Username)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	// scanner.Scan(entry.WebsiteName)
+	fmt.Println("Number of characters")
+	var chars string
+	n, err := fmt.Scanln(&chars)
+	if err != nil || n > 3 {
+		fmt.Println("Error: can't be higher than 255 and ", err)
+		return
+	}
+	convertedNum, err := strconv.Atoi(chars)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	if convertedNum > 255 || convertedNum < 8 {
+		fmt.Println("Error: can't be higher than 255 or lower than 8")
+		return
+	}
+
+	entry.Password = randomize(uint8(convertedNum))
+}
+
+func randomize(numLetters uint8) string {
 	rand.Seed(time.Now().UnixNano())
 	var password []string
-	for i := 0; i < numLetters; i++ {
+	var i uint8
+	for i = 0; i < numLetters; i++ {
 		rand := rand.Intn(126-33) + 33
 		letter := fmt.Sprintf("%c", rand)
 		password = append(password, string(letter))
